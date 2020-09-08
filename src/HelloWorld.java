6970f8380f36b1c40c2e1692e6c7566de87e80cb
hitom <hitomi.hoshino@gmail.com>
2020-09-08 12:12:50 +0200

    Hitomi changed the code: Counting from 21 to 30

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,12 +1,17 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        /*System.out.println("Hello World");
         System.out.println("Teller til ti");
         for (int i = 1; i<= 10; i++){
             System.out.println(i);
         }   System.out.println("Teller til tyve");
         for (int i = 11; i<= 20; i++){
             System.out.println(i);
+        }*/
+
+        for(int i = 21; i <= 30; i++){
+            System.out.println(i);
+            //System.out.println("Count to 30");
         }
     }
 }
